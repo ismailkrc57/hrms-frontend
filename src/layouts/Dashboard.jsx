39bfdc39aa62e82a30f4aab5a6e32cd6a9a47a6d
ismailkrc57 <ismailkrc57@gmail.com>
2021-08-28 15:30:23 +0300

    import hatası düzeltildi

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import JobSeekerList from "../pages/JobSeekerList";
 import JobPositionList from "../pages/JobPositionList";
+import JobAdvertisementList from "../pages/JobAdvertisementList";
 function Dashboard(props) {
   return (
     <div>
